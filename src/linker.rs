4a243eb80342a29617c9037cf8d2c68321a4ff8c
Jordan West <jordwest@gmail.com>
2019-03-13 07:30:39 +1100

    symbol store impl

--- src/linker.rs
+++ src/linker.rs
@@ -1,5 +1,6 @@
 use crate::ast::*;
 use crate::parser::parse;
+use std::borrow::Cow;
 use std::collections::HashMap;
 use std::fs;
 use std::path::PathBuf;
@@ -23,11 +24,44 @@ pub struct Symbol {
     pub symbol_definition: SymbolDefinition,
 }
 
-pub struct SymbolStore {}
+pub struct SymbolStore<'a> {
+    /// Keyed by (namespace, name)
+    symbols: HashMap<(Cow<'a, String>, Cow<'a, String>), Symbol>,
+}
 
-pub struct ModuleResult {
+impl<'a> SymbolStore<'a> {
+    fn new() -> Self {
+        SymbolStore {
+            symbols: HashMap::new(),
+        }
+    }
+
+    fn add_symbol(store: &mut SymbolStore, symbol: Symbol) {
+        let key = (
+            Cow::Borrowed(&symbol.namespace),
+            Cow::Borrowed(&symbol.name),
+        );
+        store.symbols.insert(key, symbol);
+    }
+
+    fn get_symbol(store: &mut SymbolStore, namespace: Cow<String>, name: Cow<String>) {
+        let key = (namespace, name);
+        store.symbols.get(&key);
+    }
+}
+
+#[test]
+fn test_symbol_store() {
+    let symbol = Symbol {
+        namespace: String::from("App.Namespace"),
+        name: String::from("Message"),
+        symbol_definition: SymbolDefinition::Primitive,
+    };
+}
+
+pub struct ModuleResult<'a> {
     /// Symbols defined in this module and all imported modules
-    pub symbol_lookup: SymbolStore,
+    pub symbol_lookup: SymbolStore<'a>,
 
     pub entry_file: XtFile,
 }
@@ -58,12 +92,12 @@ fn test_load_file_and_imports() {
 }
 
 /// This is the main function used to load and parse `xt` files and their imports.
-pub fn load_module(filename: String) -> ModuleResult {
+pub fn load_module<'a>(filename: String) -> ModuleResult<'a> {
     let entry_file_src = fs::read_to_string(&filename).unwrap();
     let entry_file = parse(&entry_file_src);
     // let imports: Vec<XtFile> = vec![];
     ModuleResult {
-        symbol_lookup: SymbolStore {},
+        symbol_lookup: SymbolStore::new(),
         entry_file,
     }
 }
