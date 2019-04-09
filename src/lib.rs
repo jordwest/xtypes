71540a989309c4ca9fe8d1c3c063af4e38f54eb8
Jordan West <jordwest@gmail.com>
2019-04-10 08:23:03 +1000

    WIP module loader

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod ast;
 pub mod linker;
+pub mod module_loader;
 pub mod parser;
 pub mod writers;
---
+++ src/module_loader.rs
@@ -0,0 +1,37 @@
+use crate::ast::{Message, ModuleImport, XtFile};
+use std::collections::BTreeMap;
+use std::path::PathBuf;
+
+struct ScopeItem<'a> {
+    symbol: &'a Message,
+    containing_module: &'a XtFile,
+    fully_qualified_name: String,
+    use_statement: &'a ModuleImport,
+}
+
+/// Keeps track of symbols in scope
+struct Scope<'a> {
+    symbol_map: BTreeMap<String, ScopeItem<'a>>,
+}
+
+trait ModuleLoader {
+    fn load_module(&mut self, name: AsRef<str>) -> XtFile;
+    fn load_imports(&mut self, module: &XtFile) -> BTreeMap<String, XtFile>;
+}
+
+pub struct FileModuleLoader {
+    search_paths: Vec<PathBuf>,
+}
+
+impl FileModuleLoader {
+    pub fn new() -> Self {
+        FileModuleLoader {
+            search_paths: Vec::new(),
+        }
+    }
+}
+impl ModuleLoader for FileModuleLoader {
+    fn load_module(&self, name: AsRef<str>) -> XtFile {
+        self
+    }
+}
